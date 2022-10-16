21199bd9a0a5c0da7b95831e9e4a262e39dd390e
VerxuSs <113851748+VerxuSs@users.noreply.github.com>
2022-10-16 17:33:15 +0200

    Version 1.1 (option to open in the windows Explorer)

--- test_dir.py
+++ test_dir.py
@@ -13,8 +13,8 @@ for path, dirs, files in os.walk(str(folder_path).strip()):
         if filename_search.upper() in filename.upper():
             flag = 1
             path_file = path
-            path_file = path_file + "\\" + filename
-            paths.append(path_file)
+            path_file_all = path_file + "\\" + filename
+            paths.append(path_file_all)
 if flag == 1:
     print("Founded from this paths : ")
     i = 0
@@ -22,12 +22,19 @@ if flag == 1:
         for path in paths:
             i += 1
             print(i, "-", path, "\n")
-        num = input("Which file do you want to open (just do enter if you want to exit)")
+            
+        num = input("Which file do you want to open in explorer or open(just do enter if you want to exit)")
         file = paths[int(num)-1]
-        root, extension = os.path.splitext(file)
-        if num != "":
+        print("You want to use this file : ", file)
+        print("What do you want to do with it : ")
+        ouvrir_lire = input("1. Open in the Explorer \n2. Open it : ")
+        if ouvrir_lire == "1":
+            print(path_file)
+            subprocess.check_call(('explorer', path_file), shell = True)
+        else:
             subprocess.check_call(('start', "", "/max", file), shell = True)
             print("Open")
+            
     else:
         file = paths[0]
         print(paths[0])
